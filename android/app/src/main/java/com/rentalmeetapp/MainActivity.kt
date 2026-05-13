db05a2bd60efbbc349c74959d2513e18ad38c015
Sahil.Shivnani <sahilshivnani68@gmail.com>
2026-05-13 13:44:29 +0530

    #package name updated

--- android/app/src/main/java/com/rentalmeetapp/MainActivity.kt
+++ android/app/src/main/java/com/rentalmeetapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rentalmeet
+package com.rentalmeetapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/rentalmeetapp/MainApplication.kt
+++ android/app/src/main/java/com/rentalmeetapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.rentalmeet
+package com.rentalmeetapp
 
 import android.app.Application
 import com.facebook.react.PackageList
